c05ef5ab852f8b93a6d3138b3bc335b285636b84
Matteo <matteoglarey92@gmail.com>
2018-09-20 11:36:57 +0200

    Picture QR optimize

--- WcfService/Picture.cs
+++ WcfService/Picture.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
@@ -44,8 +45,7 @@ namespace WcfService
 //            Bitmap grayBM = MakeGrayscale3(bitMap);
   //          Bitmap newBM = AdjustContrast(grayBM, 100.0f);
 
-            image = (Image)bitMap;
-            //Image imageGray = (Image)grayBM;
+            image = (Image)MakeGrayscale3(bitMap);
             //Image imageFinal = (Image)newBM;
 
             String path = System.Web.Hosting.HostingEnvironment.ApplicationPhysicalPath + "Images\\" + name + ".jpg";
@@ -99,61 +99,93 @@ namespace WcfService
             return newBitmap;
         }
 
-        private static Bitmap AdjustContrast(Bitmap Image, float Value)
+        /// <summary>
+        /// Resize the image to the specified width and height.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="width">The width to resize to.</param>
+        /// <param name="height">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        public static Bitmap ResizeImage(Image image, int width, int height)
         {
-            Value = (100.0f + Value) / 100.0f;
-            Value *= Value;
-            Bitmap NewBitmap = (Bitmap)Image.Clone();
-            BitmapData data = NewBitmap.LockBits(
-                new Rectangle(0, 0, NewBitmap.Width, NewBitmap.Height),
-                ImageLockMode.ReadWrite,
-                NewBitmap.PixelFormat);
-            int Height = NewBitmap.Height;
-            int Width = NewBitmap.Width;
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
 
-            unsafe
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
             {
-                for (int y = 0; y < Height; ++y)
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
                 {
-                    byte* row = (byte*)data.Scan0 + (y * data.Stride);
-                    int columnOffset = 0;
-                    for (int x = 0; x < Width; ++x)
-                    {
-                        byte B = row[columnOffset];
-                        byte G = row[columnOffset + 1];
-                        byte R = row[columnOffset + 2];
-
-                        float Red = R / 255.0f;
-                        float Green = G / 255.0f;
-                        float Blue = B / 255.0f;
-                        Red = (((Red - 0.5f) * Value) + 0.5f) * 255.0f;
-                        Green = (((Green - 0.5f) * Value) + 0.5f) * 255.0f;
-                        Blue = (((Blue - 0.5f) * Value) + 0.5f) * 255.0f;
-
-                        int iR = (int)Red;
-                        iR = iR > 255 ? 255 : iR;
-                        iR = iR < 0 ? 0 : iR;
-                        int iG = (int)Green;
-                        iG = iG > 255 ? 255 : iG;
-                        iG = iG < 0 ? 0 : iG;
-                        int iB = (int)Blue;
-                        iB = iB > 255 ? 255 : iB;
-                        iB = iB < 0 ? 0 : iB;
-
-                        row[columnOffset] = (byte)iB;
-                        row[columnOffset + 1] = (byte)iG;
-                        row[columnOffset + 2] = (byte)iR;
-
-                        columnOffset += 4;
-                    }
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
                 }
             }
 
-            NewBitmap.UnlockBits(data);
-
-            return NewBitmap;
+            return destImage;
         }
 
+        /*        private static Bitmap AdjustContrast(Bitmap Image, float Value)
+                {
+                    Value = (100.0f + Value) / 100.0f;
+                    Value *= Value;
+                    Bitmap NewBitmap = (Bitmap)Image.Clone();
+                    BitmapData data = NewBitmap.LockBits(
+                        new Rectangle(0, 0, NewBitmap.Width, NewBitmap.Height),
+                        ImageLockMode.ReadWrite,
+                        NewBitmap.PixelFormat);
+                    int Height = NewBitmap.Height;
+                    int Width = NewBitmap.Width;
+
+                    unsafe
+                    {
+                        for (int y = 0; y < Height; ++y)
+                        {
+                            byte* row = (byte*)data.Scan0 + (y * data.Stride);
+                            int columnOffset = 0;
+                            for (int x = 0; x < Width; ++x)
+                            {
+                                byte B = row[columnOffset];
+                                byte G = row[columnOffset + 1];
+                                byte R = row[columnOffset + 2];
+
+                                float Red = R / 255.0f;
+                                float Green = G / 255.0f;
+                                float Blue = B / 255.0f;
+                                Red = (((Red - 0.5f) * Value) + 0.5f) * 255.0f;
+                                Green = (((Green - 0.5f) * Value) + 0.5f) * 255.0f;
+                                Blue = (((Blue - 0.5f) * Value) + 0.5f) * 255.0f;
+
+                                int iR = (int)Red;
+                                iR = iR > 255 ? 255 : iR;
+                                iR = iR < 0 ? 0 : iR;
+                                int iG = (int)Green;
+                                iG = iG > 255 ? 255 : iG;
+                                iG = iG < 0 ? 0 : iG;
+                                int iB = (int)Blue;
+                                iB = iB > 255 ? 255 : iB;
+                                iB = iB < 0 ? 0 : iB;
+
+                                row[columnOffset] = (byte)iB;
+                                row[columnOffset + 1] = (byte)iG;
+                                row[columnOffset + 2] = (byte)iR;
+
+                                columnOffset += 4;
+                            }
+                        }
+                    }
+
+                    NewBitmap.UnlockBits(data);
+
+                    return NewBitmap;
+                }
+        */
         public string getUrl()
         {
             return url;
@@ -177,6 +209,11 @@ namespace WcfService
         public int ScanQR()
         {
             var barcodeReader = new BarcodeReader();
+            barcodeReader.Options.PossibleFormats = new List<BarcodeFormat>();
+            barcodeReader.Options.PossibleFormats.Add(BarcodeFormat.QR_CODE);
+            barcodeReader.AutoRotate = false;
+            barcodeReader.Options.TryHarder = true;
+            
             // create an in memory bitmap
             var barcodeBitmap = (Bitmap)Bitmap.FromFile(path);
             // decode the barcode from the in memory bitmap
